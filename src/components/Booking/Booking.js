f5dc0144c115d879a89a6c87622084311d100b33
a1barif2h <a1barif2h@gmail.com>
2020-09-16 01:53:28 +0600

    Starting assignment 9

---
+++ src/components/Booking/Booking.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Booking.css";
+
+const Booking = () => {
+  return (
+    <div>
+      <h1>this is booking</h1>
+    </div>
+  );
+};
+
+export default Booking;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <div>
+      <h1>this is search</h1>
+    </div>
+  );
+};
+
+export default Search;
